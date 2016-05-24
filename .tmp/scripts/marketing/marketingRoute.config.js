3468f4f6106c571bb49c35ef665f78d9fe1b0625
Kelly Talley <kcutchen@gmail.com>
2016-05-24 15:43:56 -0500

    More styling, adding category pages

--- .tmp/scripts/marketing/marketingRoute.config.js
+++ .tmp/scripts/marketing/marketingRoute.config.js
@@ -21,6 +21,34 @@
           templateUrl: 'views/marketing/main-shop.html'
         }
       }
+    }).state('base.shop-concrete', {
+      url: 'shop-concrete',
+      views: {
+        'view': {
+          templateUrl: 'views/marketing/shop-concrete.html'
+        }
+      }
+    }).state('base.shop-decor', {
+      url: 'shop-decor',
+      views: {
+        'view': {
+          templateUrl: 'views/marketing/shop-decor.html'
+        }
+      }
+    }).state('base.shop-garden', {
+      url: 'shop-garden',
+      views: {
+        'view': {
+          templateUrl: 'views/marketing/shop-garden.html'
+        }
+      }
+    }).state('base.shop-paintings', {
+      url: 'shop-paintings',
+      views: {
+        'view': {
+          templateUrl: 'views/marketing/shop-paintings.html'
+        }
+      }
     }).state('base.product', {
       url: 'product/:id',
       views: {
